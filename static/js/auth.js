7551371d2139f5343a779ab6fa9275622d34f645
Ahmed AlMerbati <aaaalmerbati@gmail.com>
2026-08-20 01:30:56 +0300

    added auth.js

---
+++ static/js/auth.js
@@ -0,0 +1,16 @@
+async function login(username, password) {
+  const url = "https://learn.reboot01.com/api/auth/signin";
+  const credentials = btoa(username + ":" + password);
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + credentials,
+    },
+  });
+  if (response.ok) {
+    const token = await response.text();
+    return token;
+  } else {
+    throw new Error(`Login failed: ${response.status} ${response.statusText}`);
+  }
+}
