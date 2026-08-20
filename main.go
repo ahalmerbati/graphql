21e96521157e8aacea79f9b1a1ef288d2b25e9a5
Ahmed AlMerbati <aaaalmerbati@gmail.com>
2026-08-20 04:22:59 +0300

    created go server logic

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server has started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- static/js/profile.js
+++ static/js/profile.js
@@ -7,18 +7,18 @@ async function loadProfile() {
   // fetch and display user info
   const userDisplay = document.getElementById("user");
   const userData = await fetchUser();
-  userDisplay.textContent = JSON.stringify(userData, null, 2);
+  userDisplay.textContent = userData.data.user[0].login;
 
   // fetch and display xp info
   const xpDisplay = document.getElementById("xp");
   const xpData = await fetchXP();
   const xp = sumXP(xpData);
-  xpDisplay.textContent = JSON.stringify(xp);
+  xpDisplay.textContent = xp.toLocaleString();
 
   // fetch and display audit info
   const auditDisplay = document.getElementById("audit");
   const auditData = await fetchAuditRatio();
-  auditDisplay.textContent = JSON.stringify(auditData);
+  auditDisplay.textContent = auditData.data.user[0].auditRatio.toFixed(2);
 }
 loadProfile();
 
